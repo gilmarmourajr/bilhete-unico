d2bfe4a03c8bff026b8ba29c74075ceb6792184c
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-03-18 11:14:23 -0300

    Add Bilhete methods

--- src/Bilhete.java
+++ src/Bilhete.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Bilhete {
@@ -5,6 +6,7 @@ public class Bilhete {
     double saldo;
     Usuario usuario;
     Random rd = new Random();
+    DecimalFormat fM = new DecimalFormat("R$##0.00");
 
     public Bilhete(String nomeDoUsuario, String cpf, String perfil) {
         this.numero = rd.nextInt(1000, 9999);
@@ -12,8 +14,27 @@ public class Bilhete {
         this.usuario = new Usuario(nomeDoUsuario, cpf, perfil);
     }
 
+    public void carregar(double carga) {
+        if(carga > 0) {
+            this.saldo += carga;
+        }
+    }
+
+    public void consultar() {
+        System.out.println("Saldo atual: " +fM.format(this.saldo));
+    }
+
+    public void passarCatraca() {
+        double result = this.saldo - Main.tarifa;
+        if (result < 0) {
+            System.out.println("Saldo insuficiente!");
+        } else {
+            this.saldo = result;
+        }
+    }
+
     @Override
     public String toString() {
-        return "Bilhete N° " + this.numero + " - Saldo: " + this.saldo + "\n" + this.usuario;
+        return "Bilhete N° " + this.numero + " - Saldo: " + fM.format(this.saldo) + "\n" + this.usuario;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,21 @@ public class Main {
         Bilhete a = new Bilhete("Gil", "123.456.789-00", "Estudante");
         Bilhete b = new Bilhete("Fernando", "321.654.987-00", "Comum");
         System.out.println(a);
+        System.out.println(b);
+        a.carregar(50);
+        b.carregar(15);
+        a.consultar();
+        b.consultar();
+
+        a.passarCatraca();
+        b.passarCatraca();
+        a.consultar();
+        b.consultar();
+
+        b.passarCatraca();
+        b.passarCatraca();
+        b.passarCatraca();
+        a.consultar();
+        b.consultar();
     }
 }
